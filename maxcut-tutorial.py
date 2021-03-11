c588e0b47087dfa3d4564eb15ae9c3c710d3c029
Felix Truger <felix.truger@iaas.uni-stuttgart.de>
2021-03-11 22:04:59 +0100

    Add grid search for angles gamma, beta

--- maxcut-tutorial.py
+++ maxcut-tutorial.py
@@ -6,6 +6,7 @@ from graphGenerator import GraphGenerator, GraphPlotter
 from QAOACircuitGenerator import QAOACircuitGenerator
 import matplotlib.pyplot as plt 
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from matplotlib import cm
 from scipy.optimize import minimize
 
 from qiskit import Aer, IBMQ, execute
@@ -16,6 +17,7 @@ from qiskit.visualization import plot_histogram
 from qiskit.test.mock import FakeBoeblingen, FakeYorktown
 import cvxgraphalgs as cvxgr
 from numpy import dtype
+from copy import deepcopy
 
 # Compute the value of the cost function
 def cost_function_C(x,G):
@@ -37,7 +39,7 @@ def cost_function_C(x,G):
 def runQaoa(input, Graph, approximation_List, p):
     # run on local simulator
     backend = Aer.get_backend("qasm_simulator")
-    shots = 100
+    shots = 1000
     QAOA = QAOACircuitGenerator.genQAOAcircuit(input, Graph,approximation_List, p)
     TQAOA = transpile(QAOA, backend)
     qobj = assemble(TQAOA)
@@ -151,6 +153,35 @@ def bestGWcuts(graph, n_GW_cuts, n_best, continuous=False, epsilon=0.25):
     GW_cuts = GW_cuts[n_GW_cuts-n_best:]
     return GW_cuts
 
+def gridSearch(objective_fun, Graph, approximation_List, p, step_size=0.2, show_plot=False):
+    a_gamma         = np.arange(0, np.pi, step_size)
+    a_beta          = np.arange(0, np.pi, step_size)
+    a_gamma, a_beta = np.meshgrid(a_gamma,a_beta)
+    a_gamma, a_beta = a_gamma.flatten(), a_beta.flatten()
+
+    F1 = np.array([objective_fun([a_gamma[i], a_beta[i]], Graph, approximation_List, p) for i in range(len(a_gamma))])
+
+    # Grid search for the minimizing variables
+    result = np.where(F1 == np.amin(F1))
+    gamma, beta = a_gamma[result[0][0]], a_beta[result[0][0]]
+
+    # Plot the expetation value F1
+    if show_plot:
+        fig = plt.figure()
+        ax  = fig.gca(projection='3d')
+
+        size = len(np.arange(0, np.pi, step_size))
+        a_gamma, a_beta, F1 = a_gamma.reshape(size, size), a_beta.reshape(size, size), F1.reshape(size, size)
+        surf = ax.plot_surface(a_gamma, a_beta, F1, cmap=cm.coolwarm, linewidth=0, antialiased=True)
+
+        ax.set_zlim(np.amin(F1)-1,np.amax(F1)+1)
+        ax.zaxis.set_major_locator(LinearLocator(5))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+        plt.show()
+
+    return np.array([gamma, beta]), np.amin(F1)
+
 def compareWarmStartEnergy(graph, p_range):
     warm_means = []
     cold_means = []
@@ -172,7 +203,7 @@ def compareWarmStartEnergy(graph, p_range):
                 params_cold_optimized = minimize(objectiveFunction, params, method="COBYLA", args=(graph, None, p), options=optimizer_options)
                 warmstart.append(objectiveFunction(params_warm_optimized.x, graph, bestCuts[i,0], p))
                 coldstart.append(objectiveFunction(params_cold_optimized.x, graph, None, p))
-            print("{:.2f}%".format(100*(i+1+5*p_range.index(p))//(len(p_range)*5)))
+            print("{:.2f}%".format(100*(i+1+5*p_range.index(p))/(len(p_range)*5)))
 
         warm_means.append(np.mean(warmstart))
         cold_means.append(np.mean(coldstart))
@@ -194,22 +225,25 @@ def compareEpsilon(graph, epsilon_range):
     warm_means = []
     warm_dev = []
 
-    _bestCuts = bestGWcuts(graph, 15, 5, continuous=False, epsilon=0) # get raw solutions using epsilon = 0
-    print(_bestCuts)
+    RawBestCuts = bestGWcuts(graph, 1, 1, continuous=False, epsilon=0) # get raw solutions using epsilon = 0
+    print(RawBestCuts)
     p = 1
 
     epsilon_range = list(epsilon_range)
     for eps in epsilon_range:
         warmstart = []
         coldstart = []
-        bestCuts = np.array([[epsilonFunction(cut[0], eps), cut[1]] for cut in _bestCuts], dtype=object)
-        optimizer_options = None #({"maxiter": 10})# to limit optimizer iterations
-        for i in range(5):
+        bestCuts = np.array([[epsilonFunction(cut[0], eps), cut[1]] for cut in deepcopy(RawBestCuts)], dtype=object)
+        bestCutsParams = [gridSearch(objectiveFunction, graph, bestCuts[i,0], p)[0] for i in range(len(bestCuts))]
+        optimizer_options = ({"rhobeg": 0.1, "disp": False})#, "maxiter": 10})# to limit optimizer iterations
+        for i in range(-1,0):
+            params = bestCutsParams[i]
             for j in range(1):
-                params = np.random.default_rng().uniform(0, np.pi, size=2*p)
-                params_warm_optimized = minimize(objectiveFunction, params, method="COBYLA", args=(graph, bestCuts[i,0],p), options=optimizer_options)
+                #params = np.random.default_rng().normal(0, np.pi, size=2*p)
+                params_warm_optimized = minimize(objectiveFunction, params, method="COBYLA", args=(graph, bestCuts[i,0], p), options=optimizer_options)
                 warmstart.append(objectiveFunction(params_warm_optimized.x, graph, bestCuts[i,0], p))
-            print("{:.2f}%".format(100*(i+1+5*epsilon_range.index(eps))//(len(epsilon_range)*5)))
+                print("params optimized {} -> {}".format(params, params_warm_optimized.x))
+            print("{:.2f}%".format(100*(i+1+5*epsilon_range.index(eps))/(len(epsilon_range)*5)))
 
         warm_means.append(np.mean(warmstart))
         warm_dev.append(np.std(warmstart))
@@ -231,7 +265,7 @@ graph = GraphGenerator.genFullyConnectedGraph(14)
 GraphPlotter.plotGraph(graph)
 
 #compareWarmStartEnergy(graph, [1,3])
-compareEpsilon(graph, np.arange(0,0.35,0.05))
+compareEpsilon(graph, np.arange(0.0,0.51,0.05))
 
 # print(params.x)
 # plotCircuit(graph, params.x, p, FakeYorktown())
