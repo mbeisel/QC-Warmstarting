7d4bdbe296ff7a72f2807e15846badb42ce9486d
Martin Beisel <beiselmn@gmail.com>
2021-03-16 17:44:29 +0100

    Adding Optimizer comparison. Changing compute costs function to take most common result

--- maxcut-tutorial.py
+++ maxcut-tutorial.py
@@ -19,6 +19,8 @@ import cvxgraphalgs as cvxgr
 from numpy import dtype
 from copy import deepcopy
 
+
+
 # Compute the value of the cost function
 def cost_function_C(x,G):
     
@@ -39,7 +41,7 @@ def cost_function_C(x,G):
 def runQaoa(input, Graph, approximation_List, p):
     # run on local simulator
     backend = Aer.get_backend("qasm_simulator")
-    shots = 2000
+    shots = 10000
     QAOA = QAOACircuitGenerator.genQAOAcircuit(input, Graph,approximation_List, p)
     TQAOA = transpile(QAOA, backend)
     qobj = assemble(TQAOA)
@@ -50,21 +52,29 @@ def compute_costs(QAOA_results, G):
     # Evaluate the data from the simulator
     counts = QAOA_results.get_counts()
     avr_C       = 0
-    max_C       = [0,0]
-    
+    max_C       = [0,0,0]
+
     for sample in list(counts.keys()):
     
         # use sampled bit string x to compute C(x)
         x         = [int(num) for num in list(sample)]
+
         tmp_eng   = cost_function_C(x,G)
-        
+
         # compute the expectation value and energy distribution
         avr_C     = avr_C    + counts[sample]*tmp_eng
-        
-        # save best bit string
-        if( max_C[1] < tmp_eng):
+
+        # save most common string
+        if ( max_C[2] < counts[sample]):
+            max_C[2] = counts[sample]
             max_C[0] = sample
             max_C[1] = tmp_eng
+
+        # save best bit string
+        # if( max_C[1] < tmp_eng):
+        #     max_C[0] = sample
+        #     max_C[1] = tmp_eng
+
 
 
     M1_sampled   = avr_C/np.sum(list(counts.values()))
@@ -191,6 +201,89 @@ def gridSearch(objective_fun, Graph, approximation_List, p, step_size=0.2, show_
 
     return np.array([gamma, beta]), np.amin(F1)
 
+
+
+def compareOptimizerEnergy(graph, p_range, optimizers):
+    bestCuts = bestGWcuts(graph, 8, 5, continuous=False, epsilon=0)
+    bestCuts = np.array([[epsilonFunction(cut[0], 0.25), cut[1]] for cut in deepcopy(bestCuts)], dtype=object)
+    print(bestCuts)
+
+    p_range = list(p_range)
+    optimizers_p_values_warm = [ [ ] for j in range(len(optimizers)) ]
+    optimizers_p_values_cold = [ [ ] for j in range(len(optimizers)) ]
+    optimizers_p_std_warm = [ [ ] for j in range(len(optimizers)) ]
+    optimizers_p_runtime = [ [ ] for j in range(len(optimizers)) ]
+
+    for p in p_range:
+        warmstart = []
+        coldstart = []
+
+        optimizer_options = None #({"maxiter": 10})# to limit optimizer iterations
+        #take 3rd best cut
+        for i in range(2,3):
+            params = np.zeros(2*p)  # np.random.default_rng().uniform(0, np.pi, size=2*p)
+            for optimizer in range(len(optimizers)):
+                results = []
+                times = []
+                for j in range(1):
+                    import time
+                    t1 = time.time()
+                    params_warm_optimized = minimize(objectiveFunction, params, method=optimizers[optimizer], args=(graph, bestCuts[i,0],p), options=optimizer_options)
+                    results.append(objectiveFunctionBest(params_warm_optimized.x, graph, bestCuts[i,0], p))
+                    t2 = time.time()
+                    times.append(t2-t1)
+
+                    # params_cold_optimized = minimize(objectiveFunction, params, method=optimizers[optimizer], args=(graph, None, p), options=optimizer_options)
+                    # coldstart.append(objectiveFunctionBest(params_cold_optimized.x, graph, None, p))
+                    # optimizers_p_values_cold[optimizer].append(objectiveFunctionBest(params_cold_optimized.x, graph, None, p))
+
+                optimizers_p_values_warm[optimizer].append(np.mean(results))
+                optimizers_p_std_warm[optimizer].append(np.std(results))
+                optimizers_p_runtime[optimizer].append(np.mean(times))
+                print(optimizers_p_values_warm)
+
+
+        # with open('output.txt', 'w') as f:
+        #     print(warmstart, file=f)
+        #     print(coldstart, file=f)
+
+    print(optimizers_p_values_warm)
+    for optimizer in range(len(optimizers)):
+        print(optimizers[optimizer] + str(optimizers_p_values_warm[optimizer]))
+        with open('output.txt', 'w') as f:
+            print(optimizers[optimizer] + str(optimizers_p_values_warm)[optimizer], file=f)
+
+    # values to test graph layout
+    # optimizers_p_values_warm = [[104, 104],[102, 99],[95,104], [101, 101], [95,103]]
+    # optimizers_p_runtime = [[103, 103],[103, 99],[95,103], [101, 101], [95,103]]
+    # optimizers_p_std_warm = [[3, 1],[2.5, 0],[9,1], [1, 1], [9,1]]
+
+    #warmstartgraph
+    for optimizer in range(len(optimizers)):
+        plt.errorbar(p_range, optimizers_p_values_warm[optimizer], optimizers_p_std_warm[optimizer], marker="x", label=optimizers[optimizer])
+    plt.legend(loc="best"), plt.xlabel("p"), plt.ylabel("Cutsize"), plt.title("Optimizer comparison warmstart")
+    plt.savefig('optimizers_p_values_warm.png')
+    plt.show()
+    plt.close()
+
+
+    #coldstartgraph
+    # for optimizer in range(len(optimizers)):
+    #     plt.errorbar(p_range, optimizers_p_values_cold[optimizer], marker="x", label=optimizers[optimizer])
+    # plt.legend(loc="best"), plt.xlabel("p"), plt.ylabel("Energy"), plt.title("Optimizer comparison coldstart")
+    # plt.savefig('optimizers_p_values_cold.png')
+    # plt.show()
+    # plt.close()
+
+    #runtimegraph
+    for optimizer in range(len(optimizers)):
+        plt.errorbar(p_range, optimizers_p_runtime[optimizer], marker="x", label=optimizers[optimizer])
+    plt.legend(loc="best"), plt.xlabel("p"), plt.ylabel("Runtime in s"), plt.title("Optimizer runtime comparison (warmstart)")
+    plt.savefig('optimizers_p_runtime.png')
+    plt.show()
+    plt.close()
+
+
 def compareWarmStartEnergy(graph, p_range):
     warm_means = []
     cold_means = []
@@ -209,11 +302,11 @@ def compareWarmStartEnergy(graph, p_range):
         coldstart = []
 
         optimizer_options = None #({"maxiter": 10})# to limit optimizer iterations
-        for i in range(3,4):
+        for i in range(3,5):
 
             # bestCutsParams = [gridSearch(objectiveFunction, graph, bestCuts[i,0], p)[0] for i in range(len(bestCuts))]
             for j in range(1):
-                params = np.random.default_rng().uniform(0, np.pi, size=2*p)
+                params = np.zeros(2*p)  #np.random.default_rng().uniform(0, np.pi, size=2*p)
                 params_warm_optimized = minimize(objectiveFunction, params, method="COBYLA", args=(graph, bestCuts[i,0],p), options=optimizer_options)
                 # plotCircuit(graph, bestCuts[i,0], params_warm_optimized.x, p,)
                 params_cold_optimized = minimize(objectiveFunction, params, method="COBYLA", args=(graph, None, p), options=optimizer_options)
@@ -231,14 +324,16 @@ def compareWarmStartEnergy(graph, p_range):
         print(coldstart)
 
     print([warm_means, cold_means])
-    print([warm_max, cold_max])
+    # print([warm_max, cold_max])
     plotline, capline, barlinecols = plt.errorbar(p_range, cold_means, cold_dev, linestyle="None", marker="x", color="b")
     [(bar.set_alpha(0.5), bar.set_label("coldstarted")) for bar in barlinecols]
     plotline, capline, barlinecols = plt.errorbar(p_range, warm_means, warm_dev, linestyle="None", marker="x", color="r")
     [(bar.set_alpha(0.5), bar.set_label("warmstarted")) for bar in barlinecols]
     plt.legend(loc="best"), plt.xlabel("p"), plt.ylabel("Energy"), plt.title("Warm-started QAOA comparison")
-    plt.legend(loc="best")
     plt.show()
+
+
+
 
 def compareEpsilon(graph, epsilon_range):
     warm_means = []
@@ -277,10 +372,12 @@ def compareEpsilon(graph, epsilon_range):
 # graph = GraphGenerator.genGridGraph(4,4)
 # graph = GraphGenerator.genFullyConnectedGraph(19, [-5,10])
 # graph = GraphGenerator.genMustyGraph()
-graph = GraphGenerator.genRandomGraph(22,177)
-GraphPlotter.plotGraph(graph)
+graph = GraphGenerator.genRandomGraph(11,55)
+# GraphPlotter.plotGraph(graph)
 
-compareWarmStartEnergy(graph, [1,2])
+# compareWarmStartEnergy(graph, [1,2])
+compareOptimizerEnergy(graph, [1,2], ["Cobyla", "CG", "TNC"])
+# compareOptimizerEnergy(graph, [1,2], ["Cobyla", "Powell"])
 # compareEpsilon(graph, np.arange(0.0,0.51,0.05))
 
 # print(params.x)
@@ -288,3 +385,5 @@ compareWarmStartEnergy(graph, [1,2])
 # plotSolution(graph, params.x, p)
 # plt.plot(costs_history)
 # plt.show()
+
+
