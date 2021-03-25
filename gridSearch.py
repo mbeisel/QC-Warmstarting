cec8bc4856397161cba6911070c4e04e8374a479
Felix Truger <felix.truger@iaas.uni-stuttgart.de>
2021-03-25 17:45:12 +0100

    Modify grid search to load graph/cuts from file

--- gridSearch.py
+++ gridSearch.py
@@ -2,19 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from goemansWilliamson import bestGWcuts
-from graphGenerator import GraphGenerator
+from graphStorage import GraphStorage
 from maxcutQaoa import objectiveFunction, objectiveFunctionBest, cost_function_C
 from scipy.optimize import minimize
-
+from helperFunctions import epsilonFunction
+from datetime import datetime
 
 # Gridsearch for p = 1
-def gridSearch(objective_fun, Graph, approximation_List, p, gamma_step_size=0.5, gammaStart = 0, gammaEnd = np.pi,
-               beta_step_size = 0.5, betaStart =0, betaEnd=np.pi, plot=False, fname=None):
+def gridSearch(objective_fun, Graph, approximation_List, p, gamma_step_size=0.5, gammaStart=0, gammaEnd=np.pi,
+               beta_step_size=0.5, betaStart=0, betaEnd=np.pi, plot=False, fname=None):
 
     a_gamma = np.arange(gammaStart, gammaEnd, gamma_step_size)
     a_beta = np.arange(betaStart, betaEnd, beta_step_size)
+
     a_gamma, a_beta = np.meshgrid(a_gamma, a_beta)
-    stepAmount = a_gamma.shape
+    shape = a_gamma.shape
     a_gamma, a_beta = a_gamma.flatten(), a_beta.flatten()
 
     # energy, _, _ = objectiveFunctionBest([0, np.pi/2], Graph, approximation_List, p)
@@ -32,7 +34,7 @@ def gridSearch(objective_fun, Graph, approximation_List, p, gamma_step_size=0.5,
             F1.append(objective_fun(optimizedparams, Graph, approximation_List, p))
         else:
             F1.append(objective_fun([a_gamma[i], a_beta[i]], Graph, approximation_List, p))
-        print("Current Step: {}".format(i)) if i +1 % 10 == 0 else None
+        print("Current Step: {}".format(i+1)) if (i + 1) % 10 == 0 else None
     F1 = np.array(F1)
 
     # Grid search for the minimizing variables
@@ -45,7 +47,7 @@ def gridSearch(objective_fun, Graph, approximation_List, p, gamma_step_size=0.5,
         #ax  = fig.gca(projection='3d')
 
         # size = len(np.arange(0, amountOfSteps, step_size))
-        a_gamma, a_beta, F1 = a_gamma.reshape(stepAmount), a_beta.reshape(stepAmount), F1.reshape(stepAmount)
+        a_gamma, a_beta, F1 = a_gamma.reshape(shape), a_beta.reshape(shape), F1.reshape(shape)
         #surf = ax.plot_surface(a_gamma, a_beta, F1, cmap=cm.coolwarm, linewidth=0, antialiased=True)
 
         #ax.set_zlim(np.amin(F1)-1,np.amax(F1)+1)
@@ -61,22 +63,27 @@ def gridSearch(objective_fun, Graph, approximation_List, p, gamma_step_size=0.5,
     else:
         plt.show()
 
-    return np.array([gamma, beta]), np.amin(F1)
+    return [gamma, beta], np.amin(F1)
 
-graph = GraphGenerator.genFullyConnectedGraph(11)
-RawBestCuts = bestGWcuts(graph, 10, 2, continuous=False, epsilon=0.25, cost_fun=cost_function_C)
-print(RawBestCuts)
-print("Selected GWCut {}".format(RawBestCuts[0]))
+# from graphGenerator import GraphGenerator
+# graph = GraphGenerator.genFullyConnectedGraph(20)
+# cuts = bestGWcuts(graph, 10, 5, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
+# GraphStorage.store("graphs/fullyConnected-20-graph.txt", graph)
+# GraphStorage.storeGWcuts("graphs/fullyConnected-20-cuts.txt", cuts)
 
+graph_loaded = GraphStorage.load("graphs/fullyConnected-12-graph.txt")
+cuts_loaded = GraphStorage.loadGWcuts("graphs/fullyConnected-12-cuts.txt")
+epsilon = 0.25
+cuts_loaded = np.array([[epsilonFunction(cut[0], epsilon), cut[1]] for cut in cuts_loaded], dtype=object)
 
-# Default Gridsearch for p = 1
-# gridSearch(objectiveFunction, graph, RawBestCuts[0,0], 1, plot=True )
+print(cuts_loaded)
+print("Selected GWCut {}".format(cuts_loaded[0]))
 
-#p=1 detailed search around 0, pi/2
-# gridSearch(objectiveFunction, graph, RawBestCuts[0,0], 1, gamma_step_size=0.0002, gammaStart=-0.001, gammaEnd=0.001, beta_step_size=0.001,betaStart=np.pi/2 -0.01, betaEnd=np.pi/2 +0.01, plot=True )
-
-# Gridsearch for p=3 over the entire grid
-gridSearch(objectiveFunction, graph, RawBestCuts[0,0], 3, gamma_step_size=1, beta_step_size=1 ,plot=True )
-
-#Gridsearch for p=3 from -3.4 to -2.9 as shown in warmstarting optimization paper
-# gridSearch(objectiveFunction, graph, RawBestCuts[0,0], 3, gamma_step_size=0.1, gammaStart=-3.4, gammaEnd=-2.89, beta_step_size=0.1,betaStart=-3.4, betaEnd=-2.89, plot=True )
+filename = "results/Gridsearch-"+datetime.now().strftime("%Y-%m-%d_%H-%M")+".png"
+params, min_energy = gridSearch(objectiveFunction, graph_loaded, cuts_loaded[0][0], 1,
+                                gamma_step_size=0.1, gammaStart=0, gammaEnd=2*np.pi,
+                                beta_step_size=0.1, betaStart=0, betaEnd=np.pi,
+                                fname=filename)
+print(params)
+print(min_energy)
+print(objectiveFunctionBest(params, graph_loaded, cuts_loaded[0][0], 1, showHistogram=False))
