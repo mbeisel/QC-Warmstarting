279dc9288207109e9a4fe6e3de120fdb7b284791
Felix Truger <felix.truger@iaas.uni-stuttgart.de>
2021-03-26 17:59:47 +0100

    Really fix plot on bloch sphere

--- Blochplotting.py
+++ Blochplotting.py
@@ -18,16 +18,18 @@ y = np.zeros(20)
 z = [np.cos(th) for th in np.linspace(np.pi/3, np.pi*2/3, 20)]
 b.add_points([x, y, z])
 
-x = [np.sin(th) for th in np.linspace(np.pi*2/3, np.pi*5/3, 60)]
-print(x)
+#x = [np.sin(th) for th in np.linspace(np.pi*2/3, np.pi*4/3, 60)]
+#print(x)
 # x = np.zeros(20)
 # y = [np.sin(th) for th in np.linspace(0, np.pi*1/3, 10)] + [np.sin(th) for th in np.linspace(np.pi*1/3, 0, 10)]
-y = np.array([np.sin(th) for th in np.linspace(0, np.pi, 60)]) * 2/3
-print(y)
+y = np.array([np.sin(th) for th in np.linspace(0, np.pi, 60)]) * np.sin(np.pi*2/3)
+#print(y)
 # y = np.zeros(20)
 z = [np.cos(th) for th in np.linspace(np.pi*2/3, np.pi*2/3, 60)]
+x = (1 - np.array(y)**2 - np.array(z)**2)**(1/2)
+x = list(x[:30])+list(-1*x[30:])
 b.add_points([x, y, z])
-
+print(np.array(x)**2 + np.array(y)**2 + np.array(z)**2)
 
 
 x = [np.sin(th) for th in np.linspace(np.pi*5/3, 2*np.pi, 20)]
