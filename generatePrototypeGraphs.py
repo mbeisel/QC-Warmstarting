e41111ffdd05e717c0be305160923131f1d36ec4
Felix Truger <felix.truger@iaas.uni-stuttgart.de>
2021-06-18 11:19:57 +0200

    Added many more graphs for more experiments

--- generatePrototypeGraphs.py
+++ generatePrototypeGraphs.py
@@ -4,31 +4,92 @@ from graphGenerator import GraphGenerator, GraphPlotter
 from graphStorage import GraphStorage
 from goemansWilliamson import bestGWcuts
 from matplotlib import pyplot as plt
+from helperFunctions import hammingDistance
+import json
 
-gen_cuts = 150 # generate this many cuts
-store_cuts = 30 # store only this many of the best distinct cuts
+gen_cuts = 250 # generate this many cuts
+store_cuts = 50 # store only this many of the best distinct cuts
+path = "graphs/prototype/multiExperiment"
 
-""" generate graphs """
-for nodes in [12,24]:
+def genGraph(type="3r", nodes=12, fname="test/graph.txt", gen_cuts=10, store_cuts=5):
+    print(f"{i}: {nodes} - {type}")
+    if type == "3r":
+        graph = GraphGenerator.genRegularGraph(nodes, 3)
+    elif type == "rand":
+        graph = GraphGenerator.genRandomGraph(nodes, np.sum([i for i in range(nodes)])//2)
+    elif type == "fc":
+        graph = GraphGenerator.genFullyConnectedGraph(nodes)
+
+    cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
+    GraphStorage.store(f"{path}/{type}-{str(nodes)}-graph-{i}.txt", graph)
+    GraphStorage.storeGWcuts(f"{path}/{type}-{str(nodes)}-cuts-{i}.txt", cuts)
+
+for i in range(20):
     break # disabled
-    graph = GraphGenerator.genRegularGraph(nodes, 3)
-    cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
-    GraphStorage.store(f"graphs/prototype/3r-{str(nodes)}-graph.txt", graph)
-    GraphStorage.storeGWcuts(f"graphs/prototype/3r-{str(nodes)}-cuts.txt", cuts)
-    GraphPlotter.plotGraph(graph, printWeights=False)
+    """ generate graphs """
+    for nodes in [12,24]:
+        print(f"{i}: {nodes} - 3r")
+        graph = GraphGenerator.genRegularGraph(nodes, 3)
+        cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
+        GraphStorage.store(f"{path}/3r-{str(nodes)}-graph-{i}.txt", graph)
+        GraphStorage.storeGWcuts(f"{path}/3r-{str(nodes)}-cuts-{i}.txt", cuts)
+        #GraphPlotter.plotGraph(graph, printWeights=False)
+        
+        print(f"{i}: {nodes} - rand")
+        graph = GraphGenerator.genRandomGraph(nodes, np.sum([i for i in range(nodes)])//2)
+        cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
+        GraphStorage.store(f"{path}/rand-{str(nodes)}-graph-{i}.txt", graph)
+        GraphStorage.storeGWcuts(f"{path}/rand-{str(nodes)}-cuts-{i}.txt", cuts)
+        #GraphPlotter.plotGraph(graph, printWeights=False)
 
-    graph = GraphGenerator.genRandomGraph(nodes, np.sum([i for i in range(nodes)])//2)
-    cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
-    GraphStorage.store(f"graphs/prototype/rand-{str(nodes)}-graph.txt", graph)
-    GraphStorage.storeGWcuts(f"graphs/prototype/rand-{str(nodes)}-cuts.txt", cuts)
-    GraphPlotter.plotGraph(graph, printWeights=False)
+        print(f"{i}: {nodes} - fc")
+        graph = GraphGenerator.genFullyConnectedGraph(nodes)
+        cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
+        GraphStorage.store(f"{path}/fc-{str(nodes)}-graph-{i}.txt", graph)
+        GraphStorage.storeGWcuts(f"{path}/fc-{str(nodes)}-cuts-{i}.txt", cuts)
+        #GraphPlotter.plotGraph(graph, printWeights=False)
 
-    graph = GraphGenerator.genFullyConnectedGraph(nodes)
-    cuts = bestGWcuts(graph, gen_cuts, store_cuts, continuous=False, epsilon=0.0, cost_fun=cost_function_C)  # get raw solutions using epsilon = 0
-    GraphStorage.store(f"graphs/prototype/fc-{str(nodes)}-graph.txt", graph)
-    GraphStorage.storeGWcuts(f"graphs/prototype/fc-{str(nodes)}-cuts.txt", cuts)
-    GraphPlotter.plotGraph(graph, printWeights=False)
+initial_cuts = []
+for i in range(20):
+    for var in ["3r", "rand", "fc"]:
+        for nodes in ["12"]:
+            cuts = GraphStorage.loadGWcuts(f"{path}/{var}-{str(nodes)}-cuts-{i}.txt")
+            # print(f"{path}/{var}-{str(nodes)}-cuts-{i}.txt")
+            maxcut = cuts[-1][1]
+            if not maxcut>0:
+                print(f"{path}/{var}-{str(nodes)}-cuts-{i}.txt")
+                print(f"Division by zero ahead!")
+                genGraph(type=var, nodes=int(nodes), fname=f"{path}/{var}-{str(nodes)}-cuts-{i}.txt", gen_cuts=gen_cuts, store_cuts=store_cuts)
+                continue
+            # print(cuts[-1])
+            for j in range(len(cuts)-2, -1, -1):
+                ratio = cuts[j][1]/maxcut
+                if ratio <= 0.9:
+                    initial_cuts.append([
+                        f"{path}/{var}-{str(nodes)}-graph-{i}.txt",
+                        f"{path}/{var}-{str(nodes)}-cuts-{i}.txt",
+                        j,
+                        ratio,
+                        hammingDistance(cuts[j][0], cuts[-1][0])
+                        ])
+                    # print(f"{cuts[j]} ratio: {ratio}")
+                    if ratio < 0.7:
+                        print(f"{path}/{var}-{str(nodes)}-cuts-{i}.txt")
+                        print(f"{cuts[j]} ratio: {ratio}")
+                        genGraph(type=var, nodes=int(nodes), fname=f"{path}/{var}-{str(nodes)}-cuts-{i}.txt", gen_cuts=gen_cuts, store_cuts=store_cuts)
+                    break
 
+print(initial_cuts)
+fname = f"{path}/initial_cuts-12.txt"
+initial_cuts_file = open(fname, "w")
+json.dump(initial_cuts, initial_cuts_file)
+initial_cuts_file.close()
+
+a_file = open(fname, "r")
+output = a_file.read()
+print(output)
+
+exit()
 """ plot graphs """
 rows = [12, 24]
 columns = ["3r", "rand", "fc"]
